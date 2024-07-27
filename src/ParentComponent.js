b6fb325e2233de47e0a93e670d172c8d86f16386
rubaiyat2009 <rubaiyat2009@gmail.com>
2024-07-27 09:28:27 +0800

    commm

--- src/ParentComponent.js
+++ src/ParentComponent.js
@@ -10,7 +10,7 @@ const ParentComponent = () => {
   return (
     <KeplerGl
       id="map"
-      mapboxApiAccessToken="pk.eyJ1Ijoic2FtcGxlIiwiYSI6ImNrbzZybXNzZDA1cWMyb3Fxb3R6dmN4aXEifQ.-VoK67n-9uTF1gM7zT0n_A"       //mapboxApiAccessToken="YOUR_ACTUAL_MAPBOX_TOKEN" // Replace with your actual token
+      mapboxApiAccessToken="YOUR_ACTUAL_MAPBOX_TOKEN" // Replace with your actual token
       width={window.innerWidth}
       height={window.innerHeight}
     />
