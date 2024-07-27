2d9630aa4c7bd71a31f52b99aa4de0fe7eab0a9d
rubaiyat2009 <rubaiyat2009@gmail.com>
2024-07-27 09:47:46 +0800

    inject comp

--- src/ParentComponent.js
+++ src/ParentComponent.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import KeplerGl from 'kepler.gl';
+// Remove the import statement for injectComponents
 
-// const ParentComponent = () => {
-//     const handleCustomAction = (data) => {
-//       console.log('Data from custom component:', data);
-//     };
+import CustomSidePanel from './CustomSidePanel';
 
 const ParentComponent = () => {
+  const handleCustomAction = (data) => {
+    console.log('Data from custom component:', data);
+  };
+
   return (
     <KeplerGl
       id="map"
       mapboxApiAccessToken="YOUR_ACTUAL_MAPBOX_TOKEN" // Replace with your actual token
       width={window.innerWidth}
       height={window.innerHeight}
+      sidePanel={<CustomSidePanel onCustomAction={handleCustomAction} />}
     />
   );
 };
